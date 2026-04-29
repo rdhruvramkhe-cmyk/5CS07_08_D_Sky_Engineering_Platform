3c914a86c763f48906d97c8f9cdf2bed37a362d9
rdhruvramkhe-cmyk <rdhruvramkhe@gmail.com>
2026-04-29 13:39:33 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,15 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth import views as auth_views
-from .import views
+from . import views
+from .views import CustomLoginView
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
-
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
