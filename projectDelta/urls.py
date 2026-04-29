b93d77e506b810d6141241f9ac1064c975acf88d
rdhruvramkhe-cmyk <rdhruvramkhe@gmail.com>
2026-04-29 14:05:42 +0100

    Update urls.py

--- projectDelta/urls.py
+++ projectDelta/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from accounts.views import CustomLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # accounts: login, logout, signup
-    path('', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='home'),
+    path('', CustomLoginView.as_view(), name='home'),
     path('accounts/', include('accounts.urls')),
 
     # teams: dashboard, team list, team detail, schedule
@@ -18,3 +19,4 @@ urlpatterns = [
     # reports: dashboard, exports (Student 5)
     path('reports/', include('reports.urls')),
 ]
+
